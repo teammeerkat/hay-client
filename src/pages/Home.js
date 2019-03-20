3924648f0b3d12bd6d550968036aab36554e1bd6
cruzayye <johnnyc010392@gmail.com>
2019-03-20 10:07:58 -0700

    Poll test passes, style but still not functional for voting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Poll from '../components/poll/Poll';
 
 export default function Home() {
   return (
     <>
       <h1>Home page!!</h1>
+      <Poll question={'Is this is it Cheif?'} options={['This aint it cheif', 'This is it Cheif']} /> 
     </>
   );
 }
