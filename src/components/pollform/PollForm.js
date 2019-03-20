ccd712cc1c9625e319154f431fbe5647c6b4faf7
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-20 14:22:41 -0700

    make response method works

--- src/components/pollform/PollForm.js
+++ src/components/pollform/PollForm.js
@@ -1,20 +1,35 @@
 import React, { PureComponent } from 'react';
-
 export default class PollForm extends PureComponent {
   state = {
-    responses: [],
+    responses: ['sample reaponse1', 'sample 2'],
     question: ''
   };
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
   handleAdd = () => {
+    // Update state in this component
     const responses = this.state.responses;
-    let inputField = document.getElementsByName('response')[0];
+    const inputField = document.getElementsByName('newResponse')[0];
     responses.push(inputField.value);
     this.setState({ responses });
+
+    // Add new response to list
+    // const list = document.getElementsByName('responses')[0];
+    // const newResponse = this.makeResponse(inputField.value);
+    console.log(this.makeResponse(inputField.value));
+
+
+
+
+    // Reset the input field to empty
     inputField.value = '';
   };
+  makeResponse = text => {
+    const node = document.createElement('li');
+    node.innerHTML = text;
+    return node;
+  }
   render() {
     return (
       <>
@@ -26,9 +41,12 @@ export default class PollForm extends PureComponent {
             <label>Poll Question:
               <textarea onChange={this.handleChange} name='question'></textarea>
             </label>
+            <ol>
+
+            </ol>
             <fieldset>
-              <legend>New Responses</legend>
-              <input type='text' name='response'></input>
+              <legend>New Response</legend>
+              <input type='text' name='newResponse'></input>
               <button type='button' onClick={this.handleAdd}>Add Response</button>
             </fieldset>
             <button type='submit'>Create Poll</button>
